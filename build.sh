59ff92d5a9f1b160931d90c9726daac7bb73927b
n3ptune-plan3t <plawanjyotitalukdar@gmail.com>
2026-09-09 22:32:21 +0530

    Remove unnecessary build tools from build.sh
    Removed gcc, g++, make, cmake, rust, cargo, and systemd from the installation list.

--- build.sh
+++ build.sh
@@ -72,7 +72,6 @@ dnf install -y \
     createrepo_c \
     github-cli \
     hostname
-    gcc g++ make cmake rust cargo systemd
 
 # ============================================================
 # Create builder user
